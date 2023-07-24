86640d8f5fa452ec89135f4baf84734eac5da11c
emilydev <emilse.a.b@gmail.com>
2023-07-24 12:30:41 -0500

    se carga pdf accesible ajustado

--- src/config/global.js
+++ src/config/global.js
@@ -234,7 +234,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/pdf.pdf',
+        download: 'downloads/228138_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
